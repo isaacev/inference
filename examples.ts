f0a41789426f59ce5cd1a736dac9d2e8a8a00336
isaacev <isaacevavold@gmail.com>
2018-10-03 16:16:53 -0500

    fix template formatting

--- examples.ts
+++ examples.ts
@@ -3,8 +3,8 @@ export const DEFAULT_TEMPLATE = `<h1>{{.title}}</h1>
 {{#if .body}}
   <article>{{.body}}</article>
   <ul>
-  {{#loop .related}}
-    <li>{{.name}}</li>
-  {{/loop}}
+    {{#loop .related}}
+      <li>{{.name}}</li>
+    {{/loop}}
   </ul>
 {{/if}}`
