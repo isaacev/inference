bf9b51c75c8ca201ad479af949a4ddc3d9062684
isaacev <isaacevavold@gmail.com>
2018-10-17 15:33:28 -0500

    add error snippet to debug panel

--- src/editor.tsx
+++ src/editor.tsx
@@ -6,7 +6,7 @@ import { Editor, EditorConfiguration, EditorChange } from 'codemirror'
 import * as localforage from 'localforage'
 
 // App libraries.
-import { Point } from './points'
+import { Point, Range } from './points'
 import * as paths from './paths'
 import * as grammar from './grammar'
 import { scope, types } from './semantics'
@@ -106,12 +106,88 @@ interface ErrorMessageProps {
 }
 
 class ErrorMessage extends React.PureComponent<ErrorMessageProps> {
+  public static templateSnippet(template: string, where: Range | Point) {
+    if (where instanceof Point) {
+      where = new Range(where, where)
+    }
+
+    // Which line does the error start on?
+    const firstErrorLine = where.left.line
+    const lastErrorLine = where.right.line
+
+    // Convert template into line objects.
+    const lines = template
+      .split('\n')
+      .map((text, index) => ({ num: index + 1, text }))
+
+    // Build the error underline.
+    const underlinePadding = ' '.repeat(where.left.column - 1)
+    const underlineWidth = Math.max(where.right.column - where.left.column, 1)
+    const underline = underlinePadding + '^'.repeat(underlineWidth)
+    lines.splice(where.left.line, 0, { num: where.left.line, text: underline })
+
+    // Limit the lines shown to the error line +/- cropping.
+    const linesAbove = 2
+    const linesBelow = 0
+    const firstCroppedLine =
+      firstErrorLine > linesAbove ? firstErrorLine - linesAbove - 1 : 0
+    const lastCroppedLine =
+      lastErrorLine <= lines.length - linesBelow
+        ? lastErrorLine + linesBelow + 1
+        : lines.length
+    const cropped = lines.slice(firstCroppedLine, lastCroppedLine)
+
+    // Compute maximum gutter width.
+    const gutterWidth = Math.max(...cropped.map(({ num }) => `${num}`.length))
+
+    let before = 0
+    let snippet = ''
+    for (const { num, text } of cropped) {
+      const gutter =
+        before === num
+          ? ' '.repeat(gutterWidth)
+          : num.toString().padStart(gutterWidth)
+      before = num
+
+      const line = `${gutter} | ${text}`
+      if (snippet === '') {
+        snippet = line
+      } else {
+        snippet += '\n' + line
+      }
+    }
+
+    return snippet
+  }
+
   public render() {
+    const {
+      location: { start, end },
+    } = this.props.error
+    const range = new Range(
+      new Point(start.line, start.column),
+      new Point(end.line, end.column)
+    )
+    const snippet = ErrorMessage.templateSnippet(this.props.template, range)
     return (
       <div id="error-message">
         <h2 className="headline">Syntax error</h2>
+        <h3>{this.props.error.message}</h3>
+        <pre className="snippet">{snippet}</pre>
       </div>
     )
+  }
+}
+
+const series = (low: number, high: number): number[] => {
+  if (low > high) {
+    return []
+  } else {
+    const range = [] as number[]
+    for (let i = low; i <= high; i++) {
+      range.push(i)
+    }
+    return range
   }
 }
 
