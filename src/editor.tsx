773da4226dae40d63b395b4584ac596b00bb4e58
isaacev <isaacevavold@gmail.com>
2018-10-17 15:33:03 -0500

    remove cursor and marker tracking

--- src/editor.tsx
+++ src/editor.tsx
@@ -2,13 +2,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Controlled as CodeMirror } from 'react-codemirror2'
-import {
-  Editor,
-  EditorChange,
-  TextMarker,
-  EditorConfiguration,
-  TextMarkerOptions,
-} from 'codemirror'
+import { Editor, EditorConfiguration, EditorChange } from 'codemirror'
 import * as localforage from 'localforage'
 
 // App libraries.
@@ -21,10 +15,6 @@ const CODEMIRROR_OPTIONS: EditorConfiguration = {
   lineNumbers: true,
 }
 
-const BLOCK_MARKER_OPTIONS: TextMarkerOptions = {
-  className: 'text-marker-red',
-}
-
 interface AppProps {
   initialTemplate: string
   onChange: (template: string) => void
@@ -32,8 +22,6 @@ interface AppProps {
 
 interface AppState {
   template: string
-  cursor: Point
-  marker: TextMarker | null
   syntax: grammar.Statements | grammar.SyntaxError
 }
 
@@ -50,52 +38,17 @@ class App extends React.Component<AppProps, AppState> {
     }
   }
 
-  // private static localBlock(cur: Point, tree: grammar.Statements): ast.Node {
-  //   const recurse = (cur: Point, node: ast.Node): ast.Node | null => {
-  //     if (node instanceof grammar.Statements) {
-  //       for (const child of node.children) {
-  //         const found = recurse(cur, child)
-  //         if (found) {
-  //           return found
-  //         }
-  //       }
-  //       return null
-  //     } else if (node instanceof ast.BlockAction) {
-  //       const nodeStartsBefore = node.range.left.before(cur)
-  //       const nodeEndsAfter = node.range.right.after(cur)
-  //       if (nodeStartsBefore && nodeEndsAfter) {
-  //         for (const child of node.children) {
-  //           const found = recurse(cur, child)
-  //           if (found) {
-  //             return found
-  //           }
-  //         }
-  //         return node
-  //       }
-  //       return null
-  //     } else {
-  //       return null
-  //     }
-  //   }
-
-  //   return recurse(cur, tree) || tree
-  // }
-
   constructor(props: AppProps) {
     super(props)
 
     // Initialize state.
     this.state = {
       template: this.props.initialTemplate,
-      cursor: new Point(1, 1),
-      marker: null,
       syntax: App.parse(this.props.initialTemplate),
     }
 
     // Bind component methods.
     this.onBeforeChange = this.onBeforeChange.bind(this)
-    this.onCursorActivity = this.onCursorActivity.bind(this)
-    this.onBlur = this.onBlur.bind(this)
   }
 
   private onBeforeChange(editor: Editor, diff: EditorChange, tmpl: string) {
@@ -106,36 +59,6 @@ class App extends React.Component<AppProps, AppState> {
     this.props.onChange(tmpl)
   }
 
-  private onCursorActivity(editor: Editor) {
-    // Update state with cursor position.
-    const pos = editor.getDoc().getCursor()
-    this.setState({
-      cursor: new Point(pos.line + 1, pos.ch + 1),
-    })
-
-    // Clear the current marker if it exists.
-    if (this.state.marker) {
-      this.state.marker.clear()
-      this.setState({ marker: null })
-    }
-
-    // Highlight the current block.
-    // if (this.state.syntax instanceof grammar.Statements) {
-    //   const block = App.localBlock(this.state.cursor, this.state.syntax)
-    //   const from = block.range.left.toPosition()
-    //   const to = block.range.right.toPosition()
-    //   const marker = editor.getDoc().markText(from, to, BLOCK_MARKER_OPTIONS)
-    //   this.setState({ marker })
-    // }
-  }
-
-  private onBlur() {
-    if (this.state.marker) {
-      this.state.marker.clear()
-      this.setState({ marker: null })
-    }
-  }
-
   public render() {
     return (
       <>
@@ -143,8 +66,6 @@ class App extends React.Component<AppProps, AppState> {
           <CodeMirror
             value={this.state.template}
             onBeforeChange={this.onBeforeChange}
-            onCursorActivity={this.onCursorActivity}
-            onBlur={this.onBlur}
             options={CODEMIRROR_OPTIONS}
           />
         </div>
