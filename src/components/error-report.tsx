e06913b85a8be935ac0b2bff36852cd319276d4a
isaacev <isaacevavold@gmail.com>
2018-12-02 11:59:53 -0600

    remove old formatter

--- src/components/error-report.tsx
+++ src/components/error-report.tsx
@@ -28,77 +28,6 @@ export default class ErrorReport extends React.PureComponent<Props> {
   }
 }
 
-function toSnippet(template: string, where: Location) {
-  const firstErrorLine = where.start.line
-  const lastErrorLine = where.end.line
-
-  // Convert template into line objects.
-  const lines: { num?: number; text: string }[] = template
-    .split('\n')
-    .map((text, index) => ({ num: index + 1, text }))
-
-  // Build the error underline.
-  const underlinePadding = ' '.repeat(where.start.column - 1)
-  const underlineWidth = Math.max(where.end.column - where.start.column, 1)
-  const underline = underlinePadding + '^'.repeat(underlineWidth)
-
-  // Inject underline and error message into the template.
-  const message = 'hello world foo bar baz'
-  lines.splice(where.start.line, 0, { text: underline + ' ' + message })
-
-  // Limit the lines shown to the error line +/- cropping.
-  const linesShownBefore = 3
-  const linesShownAfter = 3
-  const firstCroppedLine =
-    firstErrorLine > linesShownBefore
-      ? firstErrorLine - linesShownBefore - 1
-      : 0
-  const lastCroppedLine =
-    lastErrorLine <= lines.length - linesShownAfter
-      ? lastErrorLine + linesShownAfter + 1
-      : lines.length
-  const cropped = lines.slice(firstCroppedLine, lastCroppedLine)
-
-  // Determine maximum gutter width.
-  const gutterMax = Math.max(...cropped.map(line => `${line.num || 0}`.length))
-
-  // Combine all lines into a single snippet.
-  const formattedLines = cropped.map((line, i) => {
-    const classes =
-      line.num === undefined
-        ? 'underline'
-        : classnames('code', {
-            'is-error': line.num >= firstErrorLine && line.num <= lastErrorLine,
-          })
-
-    const gutter = !line.num
-      ? ' '.repeat(gutterMax)
-      : `${line.num}`.padStart(gutterMax)
-
-    return (
-      <span className={classes}>
-        <span className="gutter">{gutter}</span>
-        <span className="border">{' | '}</span>
-        <span className="text">{line.text}</span>
-      </span>
-    )
-  })
-  return putBetween(formattedLines, <br />).map(addKeys)
-}
-
-function putBetween<T>(arr: T[], glue: T): T[] {
-  return arr.reduce(
-    (acc, elem, i) => {
-      if (i > 0) {
-        return acc.concat(glue, elem)
-      } else {
-        return acc.concat(elem)
-      }
-    },
-    [] as T[]
-  )
-}
-
 const LINES_BEFORE = 3
 const LINES_AFTER = 3
 
@@ -138,7 +67,7 @@ const MarkedLine = (props: {
   </span>
 )
 
-const toSnippet2 = (template: string, { start, end }: Location) => {
+const toSnippet = (template: string, { start, end }: Location) => {
   // Split template into individual lines, each tagged with the respective line
   // number and starting offset
   const lines = stringToLines(template)
@@ -208,6 +137,19 @@ const stringToLines = (str: string, num = 1, offset = 0) => {
     offset += text.length
     return line
   })
+}
+
+function putBetween<T>(arr: T[], glue: T): T[] {
+  return arr.reduce(
+    (acc, elem, i) => {
+      if (i > 0) {
+        return acc.concat(glue, elem)
+      } else {
+        return acc.concat(elem)
+      }
+    },
+    [] as T[]
+  )
 }
 
 function addKeys(elem: JSX.Element, index: number) {
